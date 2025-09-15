6dc41f7c310c389fe8464bf63ec2b9fbe9cb3099
Driss <rossy5502@hotmail.com>
2025-09-15 10:27:18 +0100

    intial commit

--- Days1-10/Day-9/stage-1/main.py
+++ Days1-10/Day-9/stage-1/main.py
@@ -2,16 +2,25 @@ from art import logo
 print(logo)
 
 bids = {}
-bidding_finished = False
-while not bidding_finished:
+continue_bidding=True
+while continue_bidding:
     name = input("What is your name?: ").title()
     price = int(input("What is your bid?: $"))
     bids[name] = price
     should_continue = input("Are there any other bidders? Type 'yes' or 'no'.").lower()
     if should_continue == "no":
-        bidding_finished = True
+        continue_bidding = False
     elif should_continue == "yes":
         print("\n" * 20)
 
-winner = max(bids, key=bids.get) # Find the key with the maximum value
+winner = ""
+max_bid = 0
+for name in bids:
+    if bids[name] > max_bid:
+        max_bid = bids[name]
+        winner=name
+#or use
+#winner = max(bids, key=bids.get) # Find the key with the maximum value
+
+
 print(f"The winner is {winner} with a bid of ${bids[winner]}")
