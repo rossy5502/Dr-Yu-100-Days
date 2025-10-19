2499e6c8b99ac03c7b654c6f2cc268e341ce336d
Driss <rossy5502@hotmail.com>
2025-10-19 17:42:45 +0100

    initial

---
+++ Days_31-40/Day-34/main.py
@@ -0,0 +1,30 @@
+from quizinterface import QuizInterface
+from quiz_brain import QuizBrain
+import requests
+import html
+
+def main():
+    # Fetch questions from the API
+    request = requests.get("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=boolean")
+    request.raise_for_status()
+    data = request.json()
+    question_data = data["results"]
+
+    # Prepare question bank
+    question_bank = []
+    for question in question_data:
+        question_text = html.unescape(question["question"])
+        question_answer = html.unescape(question["correct_answer"])
+        question_bank.append((question_text, question_answer))
+
+    quiz = QuizBrain(question_bank)
+    quiz_interface = QuizInterface(quiz)
+
+    while quiz.still_has_questions():
+        quiz_interface.get_next_question()
+
+
+if __name__ == "__main__":
+    main()
+
+    
